5410630de7ecbcdbfbe9f95472a96bdd87ab42bc
Tamas Zentai <zentaitamas@gmail.com>
2021-09-15 15:50:42 +0100

    add connection to mongodb atlas by mongoose and using dotenv

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello from the server!');
+})
+
+mongoose.connect(process.env.DATABASE_CONNECTION, () => {
+    console.log('connected to DB!');
 })
 
 app.listen(5000);
